8f6b6e6d9eacfc715a8ebfbff0fb938d537f0d97
aperov <perov@reksoft.ru>
2018-09-19 18:46:27 +0300

    test commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const EventEmmitter = require('events').EventEmitter;
 
 const app = express();
 const event = new EventEmmitter();
+
+mongoose.connect("mongodb://182485.simplecloud.ru:27017/test", { useNewUrlParser: true })
+  .then(() => {
+    console.log('connect');
+  })
+  .catch(err => {
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+  });
 
 setTimeout(() => event.emit('custom'), 3700);
 
